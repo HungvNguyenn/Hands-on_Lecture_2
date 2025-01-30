0e171689e6acf91d09264aa72f68986072ac4759
Hung Nguyen <hungnguyen210703@gmail.com>
2025-01-30 12:31:28 -0800

    updated pascal_triangle

--- pascal_triangle.java
+++ pascal_triangle.java
@@ -8,11 +8,15 @@ public class pascal_triangle {
         List<List<Integer>> triangle = new ArrayList<>();
 
         for(int i = 0; i < numRows; i++){
+            //creating another list for each row, so that it to be added to triangle
             List<Integer> row = new ArrayList<>();
+
+            //the pascal always start at 1, so coefficient is 1 to be added at the beginning of the row
             int coefficient = 1;
 
             for(int j = 0; j <= i; j++){
                 row.add(coefficient);
+                //using the property of binomial coefficient to directly compute value by using C(i,j) instead of needing previous row
                 coefficient = coefficient * (i - j) / (j + 1);
             }
             triangle.add(row);
